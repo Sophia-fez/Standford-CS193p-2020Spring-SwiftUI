72127cd96cf15a8544dba85a9dece27825fda286
Sophia-fez <1021867858@qq.com>
2020-06-29 19:44:49 +0800

    MyDemo_4

--- 04. Grid + enum + Optionals/MyDemo_4/ContentView.swift
+++ 04. Grid + enum + Optionals/MyDemo_4/ContentView.swift
@@ -6,14 +6,11 @@ struct ContenView: View{
 	@ObervedObject var viewModel: EmojiMemoryGame
 
 	var body: some View{
-		HStack{
-			ForEach(viewModel.card){ card in	//Array中获取，所以不需要range了
-				//onTapGesture(perform: () -> Void)没有参数也没有返回值所以可以省略掉
-				//onTapGesture(perform: {viewModel.choose(card: card)})只有一个argument，省略
-				CardView(card: card).onTapGesture{
-					self.viewModel.choose(card: card) //要加self否则报错
-				}
+		Grid(viewModel.cards){ card in
+			CardView(card: card).onTapGesture{
+				self.viewModel.choose(card: card)
 			}
+			.padding(5) //增加卡片之间的空隙
 		}
 			.padding()
 			.foregroundColor(Color.orange)
@@ -30,16 +27,17 @@ struct CardView: View{
 	}
 
 	func body(for size: CGSize) -> some View{
-		//现在不是嵌入geometry的所以不需要self.了
 		ZStack{
 			if card.isFaceUp{
 				RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
 				RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
 				Text(card.content)
 			}else{
-				RoundedRectangle(cornerRadius: cornerRadius).fill() 
+				//swiftUI里的empty view，如果卡片匹配就将它们从屏幕上清楚
+				if !card.isMatched{
+					RoundedRectangle(cornerRadius: cornerRadius).fill() 
+				}
 			}
-			//因为没有将整个gemetry传给body只传了size所以这里不需要geometry.
 			.font(Font.system(size: fontSize(for: size))
 		}
 	}
@@ -62,53 +60,20 @@ struct ContentView_Previews: PreviewProvider{
 
 //----------------------------
 
-// //代码精简1 闭包
-// struct CardView: View{
-// 	var card: MemoryGame<String>.Card
+// //闭包的糖
+// struct ContenView: View{
+// 	//加上@ObervedObject后，每次观察对象更新了就会重新绘制UI
+// 	@ObervedObject var viewModel: EmojiMemoryGame
 
 // 	var body: some View{
-// 		GeometryReader(content: { geometry in  //(content: 可以省略，闭包的糖
-// 			ZStack{
-// 				if self.card.isFaceUp{
-// 					RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-// 					RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-// 					Text(self.card.content)
-// 				}else{
-// 					RoundedRectangle(cornerRadius: 10.0).fill() 
-// 				}
-// 				//更好的封装，把字体大小设置放到这里来
-// 				.font(Font.system(size: min(geometry.size.width, geometry.size.height) * 0.75))
-// 				//这些蓝色的数字分散在代码里其实很不好，所以可以建一个控制面板
+// 		// viewForItem:是最后一个argument所以可以省略
+// 		// items:也可以不需要，需要在Grid.swift里写init
+// 		Grid(items: viewModel.cards, viewForItem: { card in
+// 			CardView(card: card).onTapGesture{
+// 				self.viewModel.choose(card: card)
 // 			}
 // 		})
-
+// 			.padding()
+// 			.foregroundColor(Color.orange)
 // 	}
-// }
-
-
-// //代码精简2 将数字替换掉，并省略self.
-// struct CardView: View{
-// 	var card: MemoryGame<String>.Card
-
-// 	var body: some View{
-// 		GeometryReader{ geometry in
-// 			ZStack{
-// 				if self.card.isFaceUp{
-// 					RoundedRectangle(cornerRadius: self.cornerRadius).fill(Color.white)
-// 					RoundedRectangle(cornerRadius: self.cornerRadius).stroke(lineWidth: self.edgeLineWidth)
-// 					Text(self.card.content)
-// 				}else{
-// 					RoundedRectangle(cornerRadius: self.cornerRadius).fill() 
-// 				}
-// 				//更好的封装，把字体大小设置放到这里来
-// 				.font(Font.system(size: min(geometry.size.width, geometry.size.height) * self.fontScaleFactor))
-// 			}
-// 		}
-// 	}
-
-// 	//MARK: - Drawing Constants
-
-// 	let cornerRadius: CGFloat = 10.0
-// 	let edgeLineWidth: CGFloat = 3
-// 	let fontScaleFactor: CGFloat = 0.75
 // }
--- 04. Grid + enum + Optionals/MyDemo_4/MemoryGame.swift
+++ 04. Grid + enum + Optionals/MyDemo_4/MemoryGame.swift
@@ -1,35 +1,50 @@
 //model
 import Foundation
 
-struct MemoryGame<CardContent>{ //宣告CardContent是通用类型
+//加上content是Equatable的约束，否则下面func choose里"=="无法使用
+struct MemoryGame<CardContent> where CardContent: Equatable{
 	var cards: Array<Card>
 
-	//所有修改自己的func都要加上mutating
-	//内外部名称一样，用 _ 替代，这里也可以不写 _
-	mutating func choose(_ card: Card){
-		print("card chosen: \(card)")
-		let chosenIndex: Int = self.index(of: card)
-		self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
-	}
-
-	//根据id找到点击的card的index，不修改所以不需要加mutating
-	//of card就是外部和内部名称
-	func index(of card: Card) -> Int{
-		for index in 0..<self.cards.count{
-			if self.cards[index].id == card.id{
-				return index
+	//使用computed var实现
+	var indexOfTheOneAndOnlyFaceUpCard: Int?{ //可以在这里初始化为nil但是不这么做
+		get{cards.indices.filter{in cards.[$0].isFaceUp}.only}
+		set{
+			for index in cards.indices{
+				cards.[index].isFaceUp = index == newValue
 			}
 		}
-		//如果找不到这个card其实返回0是不对的，这个之后再解决
-		return 0 // TODO: bogus!
 	}
 
-	//init功能室创建，没有歧义一定是mutating的所以反而不需要加mutating
+	//这里代码要随着get和set的实现改变
+	mutating func choose(_ card: Card){
+		//if let后面不可以使用&&连接判断条件，使用逗号连接判断条件，当第一个条件满足再挨个往后判断
+		if let chosenIndex: Int = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched{
+			//两张卡片匹配
+			if let potentialMtachIndex = indexOfTheOneAndOnlyFaceUpCard{
+				if cards[chosenIndex].content == cards[potentialMtachIndex].content{
+					cards[chosenIndex].isMatched = true
+					cards[potentialMtachIndex].isMatched = true
+					self.cards[chosenIndex].isFaceUp = true //choose的那张朝上
+				}
+			}else{ //除了choose的那张其他的都要朝下
+				indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+			}
+		}
+	}
+
+	// //Grid.swift写了extension，这里的func也不需要了
+	// func index(of card: Card) -> Int{
+	// 	for index in 0..<self.cards.count{
+	// 		if self.cards[index].id == card.id{
+	// 			return index
+	// 		}
+	// 	}
+	// 	return 0 // TODO: bogus!
+	// }
+
 	init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
 		cards = Array<Card>()  //创建一个卡片
 		for pairIndex in 0...<numberOfPairsOfCards{
-			//model知道content具体是什么，用函数cardContentFactory获取content
-			//content不是变量所以改用常量，swift自动识别type，所以不需要写成let content： CardContent =
 			let content = cardContentFactory(pairIndex)
 			cards.append(Card(content: content, id: pairIndex*2))
 			cards.append(Card(content: content, idL pairIndex*2+1))
@@ -39,37 +54,89 @@ struct MemoryGame<CardContent>{ //宣告CardContent是通用类型
 	struct Card: Identifiable{
 		var isFaceUp: Bool = false
 		var isMatched: Booll = false
-		var content: CardContent  //不在意卡片上到底有什么，string、image都可以
+		var content: CardContent
 		var id: Int
 	}
 }
 
 //----------------------------
 
-// //不需要identifier版的代码
-// import Foundation
-
-// struct MemoryGame<CardContent>{ //宣告CardContent是通用类型
-// 	var cards: Array<Card>
-
-// 	func choose(card: Card){
+// // 使用option之前的版本
+// 	mutating func choose(_ card: Card){
 // 		print("card chosen: \(card)")
+// 		// let chosenIndex: Int = self.index(of: card)
+// 		let chosenIndex: Int = cards.firstIndex(matching: card)
+// 		self.cards[chosenIndex].isFaceUp = !self.cards[chosenIndex].isFaceUp
 // 	}
 
-// 	init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent){
-// 		cards = Array<Card>()  //创建一个卡片
-// 		for pairIndex in 0...<numberOfPairsOfCards{
-// 			//model知道content具体是什么，用函数cardContentFactory获取content
-// 			//content不是变量所以改用常量，swift自动识别type，所以不需要写成let content： CardContent =
-// 			let content = cardContentFactory(pairIndex)
-// 			cards.append(Card(isFaceUp: false, isMatched: false, content: content))
-// 			cards.append(Card(isFaceUp: false, isMatched: false, content: content))
+//----------------------------
+
+// //未使用computed bar的版本
+// 	var indexOfTheOneAndOnlyFaceUpCard: Int? //可以在这里初始化为nil但是不这么做
+
+// 	mutating func choose(_ card: Card){
+// 		//if let后面不可以使用&&连接判断条件，使用逗号连接判断条件，当第一个条件满足再挨个往后判断
+// 		if let chosenIndex: Int = cards.firstIndex(matching: card), !cards[chosenIndex].isFaceUp, !cards[chosenIndex].isMatched{
+// 			//两张卡片匹配
+// 			if let potentialMtachIndex = indexOfTheOneAndOnlyFaceUpCard{
+// 				if cards[chosenIndex].content == cards[potentialMtachIndex].content{
+// 					cards[chosenIndex].isMatched = true
+// 					cards[potentialMtachIndex].isMatched = true
+// 				}
+// 				indexOfTheOneAndOnlyFaceUpCard = nil
+// 			}else{ //除了choose的那张其他的都要朝下
+// 				for index in cards.indices{
+// 					cards[index].isFaceUp = false
+// 				}
+//				//indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+// 			}
+// 			self.cards[chosenIndex].isFaceUp = true  //choose的那张朝上
 // 		}
 // 	}
 
-// 	struct Card{
-// 		var isFaceUp: Bool
-// 		var isMatched: Bool
-// 		var content: CardContent  //不在意卡片上到底有什么，string、image都可以
+//----------------------------
+
+// //get set精简前的版本
+// 	var indexOfTheOneAndOnlyFaceUpCard: Int?{ //可以在这里初始化为nil但是不这么做
+// 		get{
+// 			//Array<Int>()和[Ine]()都是表示Int类型的数组
+// 			//-----这部分都写到一起
+// 			var faceUpCardIndices = [Int]() //存所有朝上的卡片
+// 			for index in cards.indices{
+// 				if cards[index].isFaceUp{
+// 					faceUpCardIndices.append[index]
+// 				}
+// 			}
+// 			//-----这部分使用extension实现
+// 			if faceUpCardIndices.count == 1{
+// 			 	return faceUpCardIndices.first
+// 			}else{
+// 			 	return nil
+// 			}
+// 			//-----
+// 		}
+// 		set{ //set的精简就是把if else改成一个判断句
+// 			for index in cards.indices{
+// 				if index = newValue{ //当发现newValue时set为true，保证唯一卡片朝上
+// 					cards.[index].isFaceUp = true //只有当有一张卡片被选中的时候index才不是nil
+// 				}else{
+// 					cards.[index].isFaceUp = false
+// 				}
+// 			}
+// 		}
 // 	}
-// }
+
+
+// //对get的精简过程
+// 		get{
+// 			//精简版本1，合并
+// 			var faceUpCardIndices = cards.indices.filter{(index) -> Bool in
+// 				return cards.[index].isFaceUp
+// 			}
+// 			//精简版本2，闭包的糖
+// 			var faceUpCardIndices = cards.indices.filter{index in cards.[index].isFaceUp}
+// 			//精简版本3，$0的用法，要改为let
+// 			let faceUpCardIndices = cards.indices.filter{in cards.[$0].isFaceUp}
+// 			//对Array extension后
+// 			cards.indices.filter{in cards.[$0].isFaceUp}.only
+// 		}
